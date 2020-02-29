7e3db41926d184bc280dfedacb86f38fe9ed5c31
peterkrauz <krausepeterm@gmail.com>
2020-02-29 17:13:52 -0300

    remove completed field from achievement model

--- achievements/models.py
+++ achievements/models.py
@@ -6,7 +6,6 @@ class Achievement(models.Model):
     title = models.TextField(max_length=256)
     rpg_id = models.ForeignKey(Rpg, on_delete=models.CASCADE, default=1)
     description = models.TextField(max_length=512)
-    completed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
